f60e5ccbd9c5e55b290ee0e9eefd515c13d42e1e
FahadKabirKsl <mdfahadkabirsezan@gmail.com>
2024-06-26 18:57:26 +0600

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import ClockTime from "./components/ClockTime";
 import Todo from "./screens/Todo";
 function App() {
   return (
     <>
-      {/* <ClockTime /> */}
       <Todo />
     </>
   );
